c2740b827865febff9e97e15fa53d01cf543c5ac
hariskhan14 <mohd_hariskhan14@hotmail.com>
2023-12-26 00:42:50 +0500

    repitition of generator init is eliminated

--- src/test/java/FizzBuzzGeneratorTest.java
+++ src/test/java/FizzBuzzGeneratorTest.java
@@ -1,32 +1,36 @@
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.Assert;
 
 public class FizzBuzzGeneratorTest {
+    //Arrange - requirements
+    //Act - performs the actions
+    //Assert - test the response
+    //Note: writing the assertion first since we know what to test and then go backwards
+
+    private FizzBuzzGenerator fizzBuzzGenerator;
+    @Before
+    public void setup() {
+        this.fizzBuzzGenerator = new FizzBuzzGenerator();
+    }
 
     @Test
     public void shouldReturnFizzIfInputIs3() {
-        //Arrange - requirements
-        //Act - performs the actions
-        //Assert - test the response
-        //Note: writing the assertion first since we know what to test and then go backwards
-
-        FizzBuzzGenerator fizzBuzzGenerator = new FizzBuzzGenerator();
-        String res = fizzBuzzGenerator.gen(3);
+        String res = this.fizzBuzzGenerator.gen(3);
         Assert.assertEquals("Fizz", res);
     }
+
     @Test
     public void shouldReturnFizzIfInputIs6() {
 
-        FizzBuzzGenerator fizzBuzzGenerator = new FizzBuzzGenerator();
-        String res = fizzBuzzGenerator.gen(3);
+        String res = this.fizzBuzzGenerator.gen(6);
         Assert.assertEquals("Fizz", res);
     }
 
     @Test
     public void shouldReturnFizzIfInputIs9() {
 
-        FizzBuzzGenerator fizzBuzzGenerator = new FizzBuzzGenerator();
-        String res = fizzBuzzGenerator.gen(3);
+        String res = this.fizzBuzzGenerator.gen(9);
         Assert.assertEquals("Fizz", res);
     }
 }
